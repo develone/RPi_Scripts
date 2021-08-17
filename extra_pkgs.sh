8ce25dc781113a9558ece23042c0188fda7b1ca7
Edward Vidal Jr <develone@sbcglobal.net>
2021-08-17 10:31:48 -0600

    bashrc needs to be copied ~/.bashrc add gdb-multiarch to pkgs to install.

---
+++ extra_pkgs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install octave  vlc kicad  hexchat  \
+ gawk gimp gtkwave iverilog clang \
+samba samba-common-bin smbclient cifs-utils \
+libftdi-dev libmpc-dev libmpfr-dev squashfs-tools bc mercurial \
+libbison-dev libgmp-dev libelf-dev ncurses-dev ctags \
+default-jdk libreadline-dev xdot graphviz minicom xpdf lrzsz \
+libusb-1.0.0 exuberant-ctags gparted ffmpeg qt5-default \
+mesa-utils libboost-all-dev freeglut3 freeglut3-dev \
+python3-dev libgtk2.0-dev libcairo2-dev libpango1.0-dev \
+libgdk-pixbuf2.0-dev libatk1.0-dev libghc-x11-dev binutils-arm-none-eabi  \
+gcc-arm-none-eabi diffuse gitk tcl-dev telnet tftp hexedit cmake flex bison \
+gtkmm-3.0 gperf build-essential qt5-default nfs-kernel-server gpac \
+libeigen3-dev libffi-dev libssl-dev python python-pip libtiff-dev ppp \
+gdb-multiarch
+#sudo apt-get remove python-configparser
+
+#sudo pip install docker-compose
+
